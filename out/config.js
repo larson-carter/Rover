d3f38b21a8c6eabef6a0e13d51f18f42e88a5d3d
NBTX <nbtx@telegenic.me>
2019-08-03 01:47:30 +0100

    Change .gitignore to TypeScript ignore

--- out/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    server: {
-        // The web port that Rover will communicate on.
-        port: 3000
-    }
-};
